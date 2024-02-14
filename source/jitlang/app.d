b98255c459b66a10f96731b884f8c0e242000848
Atila Neves <atila.neves@gmail.com>
2024-02-14 22:09:28 +0100

    jitlang.io

--- source/jitlang/app.d
+++ source/jitlang/app.d
@@ -4,49 +4,28 @@ module jitlang.app;
 void run(string[] args) {
     import jitlang.parser: Parser;
     import jitlang.backend.lightning;
-    import std.stdio: writeln;
+    import jitlang.io: log;
+    import std.stdio: writeln, stdout;
     import std.file: readText;
-    import std.datetime: Clock;
-
-    const startTime = Clock.currTime;
-
-    auto sinceStart() {
-        import std.datetime: Clock;
-        return (Clock.currTime - startTime).total!"msecs";
-    }
-
-    string secondsSinceStartString() {
-        import std.string: rightJustify;
-        import std.conv: to;
-        return ("+" ~ (sinceStart / 1000.0).to!string).rightJustify(8, ' ');
-    }
-
-    void log(T...)(auto ref T args) {
-        import std.functional: forward;
-        import std.stdio: stdout;
-        alias output = stdout;
-        output.writeln("[JIT]  ", secondsSinceStartString, "s  ", forward!args);
-        output.flush;
-    }
 
     scope(exit)
-        log("Finished");
+        stdout.log("Finished");
 
     if(args.length < 2)
         throw new Exception("Usage: jit filename");
 
     const fileName = args[1];
     const source = readText(fileName);
-    log("Read source file");
+    stdout.log("Read source file");
 
     const nodes = Parser(source).parse;
-    log("Parsed source file");
+    stdout.log("Parsed source file");
     writeln("\n", nodes, "\n");
 
     auto compiler = new JITCompiler;
-    log("Compiling...");
+    stdout.log("Compiling...");
     const symbols = compiler.compile(nodes);
-    log("Compiled");
+    stdout.log("Compiled");
 
     const fun = cast(int function(int)) symbols[0];
 
---
+++ source/jitlang/io.d
@@ -0,0 +1,29 @@
+module jitlang.io;
+
+immutable imported!"std.datetime.systime".SysTime gStartTime;
+
+
+shared static this() {
+    import std.datetime: Clock;
+    gStartTime = Clock.currTime;
+}
+
+
+void log(O, T...)(auto ref O output, auto ref T args) {
+    import std.functional: forward;
+    output.writeln("[Reggae]  ", secondsSinceStartString, "s  ", forward!args);
+    output.flush;
+}
+
+
+private string secondsSinceStartString() @safe {
+    import std.string: rightJustify;
+    import std.conv: to;
+    return ("+" ~ (sinceStart / 1000.0).to!string).rightJustify(8, ' ');
+}
+
+
+private auto sinceStart() @safe {
+    import std.datetime: Clock;
+    return (Clock.currTime - gStartTime).total!"msecs";
+}
