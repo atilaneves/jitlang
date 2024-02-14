c6ff4ada926479817db0c458cd53688b623f2511
Atila Neves <atila.neves@gmail.com>
2024-02-14 22:21:16 +0100

    Refactor

--- source/jitlang/app.d
+++ source/jitlang/app.d
@@ -1,9 +1,8 @@
 module jitlang.app;
 
-
 void run(string[] args) {
     import jitlang.parser: Parser;
-    import jitlang.backend.lightning;
+    import jitlang.backend.lightning: JITCompiler;
     import jitlang.io: log;
     import std.stdio: stdout;
     import std.file: readText;
@@ -11,11 +10,9 @@ void run(string[] args) {
     scope(exit)
         stdout.log("Finished");
 
-    if(args.length < 2)
-        throw new Exception("Usage: jit filename");
-
-    const fileName = args[1];
-    const source = readText(fileName);
+    const options = Options(args);
+    stdout.log(options.fileName);
+    const source = readText(options.fileName);
     stdout.log("Read source file");
 
     const nodes = Parser(source).parse;
@@ -29,9 +26,29 @@ void run(string[] args) {
 
     const fun = cast(int function(int)) symbols[0];
 
-    notLog(fun(2));
+    notLog(fun(options.arg));
 }
 
+private struct Options {
+
+    string fileName;
+    int arg;
+
+    this(string[] args) {
+        import std.algorithm: countUntil;
+        import std.conv: to;
+
+        if(args.length < 2)
+            throw new Exception("Usage: jit filename");
+
+        fileName = args[1];
+
+        const indexOfDashDash = args.countUntil("--");
+        arg = indexOfDashDash == -1
+            ? 7
+            : args[indexOfDashDash + 1].to!int;
+    }
+}
 
 private void notLog(A...)(auto ref A args) {
     import std.stdio: writeln;
