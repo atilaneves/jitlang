1404ecefabdd8b0113aa6b5c1b3167ee17a9d9b1
Atila Neves <atila.neves@gmail.com>
2024-02-16 17:30:28 +0100

    Remove other silly prefix

--- source/jitlang/backend/lightning.d
+++ source/jitlang/backend/lightning.d
@@ -68,29 +68,29 @@ final class JITCompiler: imported!"jitlang.ast".ASTVisitor {
 
         final switch (expr.op) with(BinaryExpression.Op) {
             case Add:
-                dem_jit_addr(R0, R0, R1);
+                addr(R0, R0, R1);
                 break;
             case Sub:
-                dem_jit_subr(R0, R0, R1);
+                subr(R0, R0, R1);
                 break;
             case Mul:
-                dem_jit_mulr(R0, R0, R1);
+                mulr(R0, R0, R1);
                 break;
             case Div:
-                dem_jit_divr(R0, R0, R1);
+                divr(R0, R0, R1);
                 break;
             case ShiftLeft:
-                dem_jit_lshr(R0, R0, R1);
+                lshr(R0, R0, R1);
                 break;
             case ShiftRight:
-                dem_jit_rshr(R0, R0, R1);
+                rshr(R0, R0, R1);
                 break;
         }
         stackPush(R0); // Push result back onto stack
     }
 
     void visit(in Literal lit) {
-        dem_jit_movi(R0, lit.value);
+        movi(R0, lit.value);
         stackPush(R0);
     }
 
@@ -105,7 +105,7 @@ private:
         import jitlang.ast: BinaryExpression, Literal;
 
         if (auto lit = cast(Literal)node) {
-            dem_jit_movi(R0, lit.value);
+            movi(R0, lit.value);
             stackPush(R0);
         } else if (auto binOp = cast(BinaryExpression) node) {
             emitAST(binOp.right);
@@ -116,16 +116,16 @@ private:
 
             switch (binOp.op) {
             case BinaryExpression.Op.Add:
-                dem_jit_addr(R0, R0, R1);
+                addr(R0, R0, R1);
                 break;
             case BinaryExpression.Op.Sub:
-                dem_jit_subr(R0, R0, R1);
+                subr(R0, R0, R1);
                 break;
             case BinaryExpression.Op.Mul:
-                dem_jit_mulr(R0, R0, R1);
+                mulr(R0, R0, R1);
                 break;
             case BinaryExpression.Op.Div:
-                dem_jit_divr(R0, R0, R1);
+                divr(R0, R0, R1);
                 break;
             default:
                 throw new Exception("Unsupported binary operation");
@@ -137,12 +137,12 @@ private:
     }
 
     void stackPush(int reg) {
-        dem_jit_stxi_i(stackPtr, FP, reg);
+        stxi_i(stackPtr, FP, reg);
         stackPtr += int.sizeof;
     }
 
     void stackPop(int reg) {
         stackPtr -= int.sizeof;
-        dem_jit_ldxi_i(reg, FP, stackPtr);
+        ldxi_i(reg, FP, stackPtr);
     }
 }
--- source/jitlang/bindings/lightning.c
+++ source/jitlang/bindings/lightning.c
@@ -9,38 +9,38 @@ const int R1 = JIT_R1;
 const int FP = JIT_FP;
 
 // copied from the header (sigh)
-int dem_jit_movi(int u, int v) {
+int movi(int u, int v) {
     return jit_movi(u, v);
 }
 
-int dem_jit_addr(int u, int v, int w) {
+int addr(int u, int v, int w) {
     return jit_addr(u, v, w);
 }
 
-int dem_jit_subr(int u, int v, int w) {
+int subr(int u, int v, int w) {
     return jit_subr(u, v, w);
 }
 
-int dem_jit_mulr(int u, int v, int w) {
+int mulr(int u, int v, int w) {
     return jit_mulr(u, v, w);
 }
 
-int dem_jit_divr(int u, int v, int w) {
+int divr(int u, int v, int w) {
     return jit_divr(u, v, w);
 }
 
-int dem_jit_stxi_i(int u, int v, int w) {
+int stxi_i(int u, int v, int w) {
     return jit_stxi_i(u, v, w);
 }
 
-int dem_jit_ldxi_i(int u, int v, int w) {
+int ldxi_i(int u, int v, int w) {
     return jit_ldxi_i(u, v, w);
 }
 
-int dem_jit_lshr(int u, int v, int w) {
+int lshr(int u, int v, int w) {
     return jit_lshr(u, v, w);
 }
 
-int dem_jit_rshr(int u, int v, int w) {
+int rshr(int u, int v, int w) {
     return jit_rshr(u, v, w);
 }
